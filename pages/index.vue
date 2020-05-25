aa057f79ebcfc58684af120178f5e8880557a072
mamoru igamoto <stream99990@yahoo.co.jp>
2020-05-26 00:57:10 +0900

    add: 動画追加、コード整形

--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,7 @@
         samutama site project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
         </a>
         <a
@@ -23,10 +19,7 @@
         >
           GitHub
         </a>
-        <nuxt-link
-          to="/movie"
-          class="button--navy"
-        >
+        <nuxt-link to="/movie" class="button--navy">
           動画ローディングサンプル
         </nuxt-link>
       </div>
@@ -35,13 +28,13 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   components: {
     Logo
   }
-}
+};
 </script>
 
 <style>
@@ -55,8 +48,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
--- pages/movie.vue
+++ pages/movie.vue
@@ -1,33 +1,33 @@
 <template>
-    <div class="container">
-        <div class="tetrominos" :class="{ fadeout: !isLoading }">
-            <div class="tetromino box1"></div>
-            <div class="tetromino box2"></div>
-            <div class="tetromino box3"></div>
-            <div class="tetromino box4"></div>
-        </div>
-        <div class="movie-box" :class="{ hide: isLoading, fadein: !isLoading }">
-            <video ref="video" src="/drift.mp4" controls autoplay></video>
-        </div>
+  <div class="container">
+    <div class="tetrominos" :class="{ fadeout: !isLoading }">
+      <div class="tetromino box1"></div>
+      <div class="tetromino box2"></div>
+      <div class="tetromino box3"></div>
+      <div class="tetromino box4"></div>
     </div>
+    <div class="movie-box" :class="{ hide: isLoading, fadein: !isLoading }">
+      <video ref="video" src="/drift.mp4" controls autoplay></video>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    head: {
-        title: '動画ローディングサンプル'
-    },
-    data () {
-      return {
-          isLoading: true,
-      }
-    },
-    mounted() {
-        this.$refs.video.addEventListener("loadeddata", () => {
-            this.isLoading = false;
-        });
-    }
-}
+  head: {
+    title: "動画ローディングサンプル"
+  },
+  data() {
+    return {
+      isLoading: true
+    };
+  },
+  mounted() {
+    this.$refs.video.addEventListener("loadeddata", () => {
+      this.isLoading = false;
+    });
+  }
+};
 </script>
 
 <style lang="scss">
@@ -39,7 +39,8 @@ $speed: 1.5s;
 
 .tetrominos {
   position: absolute;
-  top: 50%; left: 50%;
+  top: 50%;
+  left: 50%;
   transform: translate(-$h, -$w);
 }
 
@@ -47,8 +48,9 @@ $speed: 1.5s;
   width: $w;
   height: $h;
   position: absolute;
-  transition: all ease .3s;
-  background: url('data:image/svg+xml;utf-8,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 612 684"%3E%3Cpath fill="%23010101" d="M305.7 0L0 170.9v342.3L305.7 684 612 513.2V170.9L305.7 0z"/%3E%3Cpath fill="%23fff" d="M305.7 80.1l-233.6 131 233.6 131 234.2-131-234.2-131"/%3E%3C/svg%3E') no-repeat top center;
+  transition: all ease 0.3s;
+  background: url('data:image/svg+xml;utf-8,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 612 684"%3E%3Cpath fill="%23010101" d="M305.7 0L0 170.9v342.3L305.7 684 612 513.2V170.9L305.7 0z"/%3E%3Cpath fill="%23fff" d="M305.7 80.1l-233.6 131 233.6 131 234.2-131-234.2-131"/%3E%3C/svg%3E')
+    no-repeat top center;
 }
 
 .box1 {
@@ -66,62 +68,76 @@ $speed: 1.5s;
 }
 
 @keyframes tetromino1 {
-  0%, 40% { 
-    /* compose logo *//* 1 on 3 *//* L-shape */
-    transform: translate(0,0);
-  } 50% {
+  0%,
+  40% {
+    /* compose logo */ /* 1 on 3 */ /* L-shape */
+    transform: translate(0, 0);
+  }
+  50% {
     /* pre-box */
-    transform: translate($xspace,-$yspace);
-  } 60%, 100% {
-    /* box *//* compose logo */
-    transform: translate($xspace*2,0);
+    transform: translate($xspace, -$yspace);
+  }
+  60%,
+  100% {
+    /* box */ /* compose logo */
+    transform: translate($xspace * 2, 0);
   }
 }
 
 @keyframes tetromino2 {
-  0%, 20% { 
-    /* compose logo *//* 1 on 3 */
-    transform: translate($xspace*2, 0px);
-  } 40%, 100% { 
-    /* L-shape *//* box *//* compose logo */
-    transform: translate($xspace*3, $yspace);
+  0%,
+  20% {
+    /* compose logo */ /* 1 on 3 */
+    transform: translate($xspace * 2, 0px);
+  }
+  40%,
+  100% {
+    /* L-shape */ /* box */ /* compose logo */
+    transform: translate($xspace * 3, $yspace);
   }
 }
 
 @keyframes tetromino3 {
-  0% { 
+  0% {
     /* compose logo */
-    transform: translate($xspace*3, $yspace);
-  } 20%, 60% { 
-    /* 1 on 3 *//* L-shape *//* box */
-    transform: translate($xspace*2, $yspace*2);
-  } 90%, 100% { 
+    transform: translate($xspace * 3, $yspace);
+  }
+  20%,
+  60% {
+    /* 1 on 3 */ /* L-shape */ /* box */
+    transform: translate($xspace * 2, $yspace * 2);
+  }
+  90%,
+  100% {
     /* compose logo */
     transform: translate($xspace, $yspace);
   }
 }
 
 @keyframes tetromino4 {
-  0%, 60% {
-    /* compose logo *//* 1 on 3 *//* L-shape *//* box */
+  0%,
+  60% {
+    /* compose logo */ /* 1 on 3 */ /* L-shape */ /* box */
     transform: translate($xspace, $yspace);
-  } 90%, 100% { 
+  }
+  90%,
+  100% {
     /* compose logo */
     transform: translate(0, 0);
   }
 }
 
 .container {
-    display: flex;
-    width: 100%;
-    height: 100vh;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
 .hide {
-    visibility: hidden;
+  visibility: hidden;
 }
 
 @keyframes fadein {
@@ -142,7 +158,7 @@ $speed: 1.5s;
   }
 }
 
-.fadein {  
+.fadein {
   animation: fadein 3s ease 0s 1 forwards;
 }
 
