b6422816d9ed3e666139a253a585e364b700d75e
mamoru igamoto <stream99990@yahoo.co.jp>
2020-05-26 23:54:40 +0900

    add: mvの動画を追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,8 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        samutama-site
-      </h1>
-      <h2 class="subtitle">
-        samutama site project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-        <nuxt-link to="/movie" class="button--navy">
-          動画ローディングサンプル
-        </nuxt-link>
+    <div class="mv">
+      <div class="mv__videoWrap">
+        <video autoplay loop muted ref="mvVideo" class="mv__video" src="/samutama_mv.mp4"></video>
       </div>
     </div>
   </div>
@@ -33,7 +14,34 @@ import Logo from "~/components/Logo.vue";
 export default {
   components: {
     Logo
-  }
+  },
+  methods: {
+    handleResize() {
+      this.backgroundMovie()
+    },
+    backgroundMovie() {
+      // ここでブラウザの縦横のサイズを取得します。
+      var windowSizeHeight = window.outerHeight;
+      var windowSizeWidth = window.outerWidth;
+
+      // メディアの縦横比に合わせて数値は変更して下さい。(メディアのサイズが width < heightの場合で書いています。逆の場合は演算子を逆にしてください。)
+      var windowMovieSizeWidth = windowSizeHeight * 1.77777778;
+      var windowMovieSizeHeight = windowSizeWidth / 1.77777778;
+      var windowMovieSizeWidthLeftMargin = (windowMovieSizeWidth - windowSizeWidth) / 2;
+
+      if (windowMovieSizeHeight < windowSizeHeight) {
+        // 横幅のほうが大きくなってしまう場合にだけ反応するようにしています。
+        this.$refs.mvVideo.style.left = -windowMovieSizeWidthLeftMargin + 'px';
+      }
+    },
+  },
+  mounted() {
+    this.backgroundMovie()
+    window.addEventListener('resize', this.handleResize)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleResize)
+  },
 };
 </script>
 
@@ -67,5 +75,27 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+.mv__videoWrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 100vw;
+  height: 100vh;
+  opacity: .8;
+}
+
+.mv__video {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  width: auto;
+  height: 100vh;
+  min-width: 100vw;
+  min-height: 100vh;
 }
 </style>
