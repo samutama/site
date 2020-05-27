bf86b6522d42bfe6d331a7110465adbbed095554
mamoru igamoto <stream99990@yahoo.co.jp>
2020-05-28 02:10:43 +0900

    add: スタイル調整

--- components/molecules/TopMv.vue
+++ components/molecules/TopMv.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="mv">
-      <div class="mv__videoWrap relative">
-        <video autoplay loop muted ref="mvVideo" class="mv__video" src="/samutama_mv.mp4"></video>
-      </div>
-      <div class="mv__ttlWrap">
-        <h1 class="ttlWrap__txt">
-          Samurai Tamashi
-          <span class="txt__span">Official Site</span>
-        </h1>
-      </div>
+    <div class="mv__videoWrap">
+      <video
+        autoplay
+        loop
+        muted
+        ref="mvVideo"
+        class="mv__video"
+        src="/samutama_mv.mp4"
+      ></video>
     </div>
+    <div class="mv__ttlWrap">
+      <h1 class="ttlWrap__txt">
+        Samurai Tamashi
+        <span class="txt__span">Official Site</span>
+      </h1>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   methods: {
     handleResize() {
-      this.backgroundMovie()
+      this.backgroundMovie();
     },
     backgroundMovie() {
       // ここでブラウザの縦横のサイズを取得します。
@@ -26,21 +33,22 @@ export default {
       // メディアの縦横比に合わせて数値は変更して下さい。(メディアのサイズが width < heightの場合で書いています。逆の場合は演算子を逆にしてください。)
       var windowMovieSizeWidth = windowSizeHeight * 1.77777778;
       var windowMovieSizeHeight = windowSizeWidth / 1.77777778;
-      var windowMovieSizeWidthLeftMargin = (windowMovieSizeWidth - windowSizeWidth) / 2;
+      var windowMovieSizeWidthLeftMargin =
+        (windowMovieSizeWidth - windowSizeWidth) / 2;
 
       if (windowMovieSizeHeight < windowSizeHeight) {
         // 横幅のほうが大きくなってしまう場合にだけ反応するようにしています。
-        this.$refs.mvVideo.style.left = -windowMovieSizeWidthLeftMargin + 'px';
+        this.$refs.mvVideo.style.left = -windowMovieSizeWidthLeftMargin + "px";
       }
-    },
+    }
   },
   mounted() {
-    this.backgroundMovie()
-    window.addEventListener('resize', this.handleResize)
+    this.backgroundMovie();
+    window.addEventListener("resize", this.handleResize);
   },
   beforeDestroy() {
-    window.removeEventListener('resize', this.handleResize)
-  },
+    window.removeEventListener("resize", this.handleResize);
+  }
 };
 </script>
 
@@ -53,10 +61,10 @@ export default {
   position: relative;
   top: 0;
   left: 0;
-  z-index: -1;
+  z-index: 0;
   width: 100vw;
   height: 100vh;
-  opacity: .8;
+  opacity: 0.8;
   overflow: hidden;
 }
 
@@ -83,14 +91,14 @@ export default {
 
 .ttlWrap__txt {
   color: #fff;
-  font-family: 'Libre Baskerville', serif;
+  font-family: "Libre Baskerville", serif;
   font-size: 9vw;
   font-style: italic;
   font-weight: bold;
 }
 
 .txt__span {
-  font-family: 'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
   font-size: 5vw;
   font-style: normal;
   font-weight: bold;
--- components/organisms/Header.vue
+++ components/organisms/Header.vue
@@ -1,13 +1,7 @@
 <template>
   <header class="header">
-    <nuxt-link
-      to="/"
-      class="inline-block"
-    >
-      <Icon
-        name="logo"
-        class="header__logo"
-      />
+    <nuxt-link to="/" class="inline-block">
+      <Icon name="logo" class="header__logo" />
     </nuxt-link>
   </header>
 </template>
@@ -16,23 +10,25 @@
 import Icon from "~/components/atoms/Icon.vue";
 
 export default {
-    components: {
-    Icon,
-  },
-}
+  components: {
+    Icon
+  }
+};
 </script>
 
 <style>
-  .header {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-  }
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 99999;
+  background: linear-gradient(to bottom, #000000 0%, transparent 100%);
+}
 
-  .header__logo {
-    width: auto;
-    height: 60px;
-    padding: 4vw;
-  }
+.header__logo {
+  width: auto;
+  height: 60px;
+  padding: 4vw;
+}
 </style>
