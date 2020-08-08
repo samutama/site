b466fbc1b8cbddd0da4d4f6a07b17bafc24a5249
mamoru igamoto <stream99990@yahoo.co.jp>
2020-08-08 20:57:24 +0900

    fix: style修正

--- components/molecules/setting/DownForce.vue
+++ components/molecules/setting/DownForce.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center flex-wrap bg-khaki rounded-lg pt-4 pb-4">
     <h2 class="pb-4 px-4 w-full text-left font-helvetica font-black">ダウンフォース</h2>
-    <div class="flex">
+    <div class="flex justify-center">
       <p class="text-4xl font-helvetica mx-4">
         {{ front }}
       </p>
